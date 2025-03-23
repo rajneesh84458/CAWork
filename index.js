0245671284671950ae2e7ca8c8aa3cbd44d68bb3
Admin <admin@Admins-MacBook-Pro.local>
2025-03-23 13:26:46 +0530

    added pathh

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const admin = require("firebase-admin");
-
+const fs = require("fs");
+const path = require("path");
 const serviceAccount = require("./ServerKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+// Define the path to save the JSON file
+const keyFilePath = path.join(__dirname, "ServerKey.json");
+
+// Check if the environment variable exists
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+  fs.writeFileSync(keyFilePath, process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  console.log("ServerKey.json created successfully!");
+} else {
+  console.error(
+    "GOOGLE_APPLICATION_CREDENTIALS environment variable is missing!"
+  );
+}
 // Function to send a notification with additional data
 async function sendNotification(token, title, body, data = {}) {
   const message = {
